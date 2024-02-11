d47531a438135c6b16d47f644a20fda02f9c70df
660610809-kornkawin <kornkawin_th@cmu.ac.th>
2024-02-11 12:09:39 +0700

    completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -6,33 +6,25 @@ struct Rect{
 };
 
 double overlap(Rect R1, Rect R2){
-	double doty;
-	double dotx;
+	double dotx,doty;
 	double overlapArea;
-	int overlapArea2;
-	if(R2.y > R1.y){
+	if(R2.y > R1.y && R2.x > R1.x){
+		dotx = R2.x - R1.x;
 		doty = R2.y - R1.y;
 	}else{
-		doty = R1.y - R2.y;
-	}
-	if(R2.x > R1.x){
-		dotx = R2.x - R1.x;
-	}else{
 		dotx = R1.x - R2.x;
+		doty = R1.y - R2.y;
 	}
 	overlapArea = (R1.w-dotx) * (R2.h-doty);
 	if(R2.x > R1.x){
 		if(R1.x + R1.w < R2.x){
 			overlapArea = 0;
 		}
-	}else{
-		if(R2.x + R2.w < R1.x){
-			overlapArea = 0;
+	}
+	if(R2.x > R1.x){
+		if(R1.x + R1.w > R2.x + R2.w){
+			overlapArea = R2.w * R2.h;
 		}
 	}
-if(overlapArea < overlapArea+1){
-		overlapArea += 0.9;
-		overlapArea2 = overlapArea;
-		}
-	return overlapArea2;
+	return overlapArea;
 }
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect {
+    double x, y, w, h;
 };
 
-double overlap(){
+double overlap(Rect* R1, Rect* R2) {
+    double dotx, doty;
+    double overlapArea = 0;
 
+    if (R2->y > R1->y && R2->x > R1->x) {
+        dotx = R2->x - R1->x;
+        doty = R2->y - R1->y;
+    } else {
+        dotx = R1->x - R2->x;
+        doty = R1->y - R2->y;
+    }
 
+    overlapArea = (R1->w - dotx) * (R2->h - doty);
 
+    if (R2->x > R1->x) {
+        if (R1->x + R1->w < R2->x) {
+            overlapArea = 0;
+        }
+    }
+    
+    if (R2->x > R1->x) {
+        if (R1->x + R1->w > R2->x + R2->w) {
+            overlapArea = R2->w * R2->h;
+        }
+    }
+
+    return overlapArea;
 }
